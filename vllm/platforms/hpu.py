4cd4d033018e4ed9bac0f452903196895f5fae11
Rafal Litka <rafal.litka@intel.com>
2024-11-28 14:39:11 +0200

    mixtral recompilation fixes

--- vllm/platforms/hpu.py
+++ vllm/platforms/hpu.py
@@ -5,7 +5,3 @@ from .interface import Platform, PlatformEnum
 
 class HpuPlatform(Platform):
     _enum = PlatformEnum.HPU
-
-    @staticmethod
-    def inference_mode():
-        return torch.no_grad()
