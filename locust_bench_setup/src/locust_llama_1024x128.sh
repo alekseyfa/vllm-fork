f1fe30186ab47e80ec9de456359c3f3224e055d5
leopck <sckphoong@habana.ai>
2024-12-23 12:58:21 +0200

    locust llama additional setup

---
+++ locust_bench_setup/src/locust_llama_1024x128.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export MODEL="meta-llama/Llama-3.1-70B-Instruct"
+export MODEL_SERVE_NAME="meta-llama/Llama-3.1-70B-Instruct" 
+export MODE=streaming
+export WAIT_TIME=0 
+export API_TYPE=openai 
+export FILE_EXTENSION=.parquet 
+export SAMPLE_FILE=1024_meta-llama--Meta-Llama-3.1-70B-Instruct.parquet 
+export MAX_TOKEN=128
